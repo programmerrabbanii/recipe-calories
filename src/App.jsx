285a2da26790d74e9ed25de2e13cf4a7c6066ad1
programmerrabbanii <rabbanisarkar642@gmail.com>
2024-10-22 23:20:53 +0600

    added the first commit here

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="w-11/12 mx-auto">
+      <Header></Header>
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+
+const Header = () => {
+    return (
+        <div>
+            <div className="grid grid-cols-1 md:grid-cols-3 justify-between mt-4 items-center">
+                <div>
+                    <h3 className="text-2xl font-semibold ">Recipe Calories</h3>
+                </div>
+                <div>
+                    <ul className="flex gap-3">
+                        <li className="text-[20px] cursor-pointer">Home</li>
+                        <li className="text-[20px] cursor-pointer">Recipes</li>
+                        <li className="text-[20px] cursor-pointer">About</li>
+                        <li className="text-[20px] cursor-pointer">Search</li>
+
+                    </ul>
+                </div>
+                <div className="flex justify-between">
+                    <div  className="bg-gray-300 w-full px-3 mr-3 py-2 rounded-3xl">
+                        <button> <i className="fa-solid fa-magnifying-glass"></i> Search</button>
+                    </div>
+                    <span><i className=" flex justify-center items-center bg-[#0BE58A] w-10 h-10 rounded-full fa-regular fa-user"></i></span>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Header;
