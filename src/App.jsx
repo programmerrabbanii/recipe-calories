76373682cd4a3a99f045a7c5f6f6437a9298ba91
programmerrabbanii <rabbanisarkar642@gmail.com>
2024-10-23 00:23:47 +0600

    added the recipe section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Bannar from "./components/Bannar";
 import Header from "./components/Header";
+import Recepies from "./components/Recepies";
 
 const App = () => {
   return (
     <div className="w-11/12 mx-auto">
       <Header></Header>
       <Bannar></Bannar>
+      <Recepies></Recepies>
       
     </div>
   );
---
+++ src/components/Recepies.jsx
@@ -0,0 +1,10 @@
+const Recepies = () => {
+    return (
+        <div className="text-center mt-20">
+            <h2 className="text-center text-3xl font-semibold mb-5">Our Recipes</h2>
+            <p className="mb-4">Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget <br /> urna volutpat curabitur elementum mauris aenean neque. </p>
+        </div>
+    );
+};
+
+export default Recepies;
